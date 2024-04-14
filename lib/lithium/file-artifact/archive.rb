b1fb3b66c5ddef2ddba73b915d6b5cb8d23055d3
andrei <andrei.vishneuski@crystalloids.com>
2024-04-14 23:56:38 +0200

    improvements

--- lib/lithium/file-artifact/archive.rb
+++ lib/lithium/file-artifact/archive.rb
@@ -251,7 +251,7 @@ class UnzipDirectory < Directory
     end
 
     def expired?
-        !File.exists?(@source) || mtime < File.mtime(@source).to_i
+        !File.exist?(@source) || mtime < File.mtime(@source).to_i
     end
 
     def build
